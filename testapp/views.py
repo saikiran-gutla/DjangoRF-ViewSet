e9095521fd18487b35b0e2bbb846487504963b98
saikiran-gutla <saikiran.gutla183@gmail.com>
2020-10-06 12:45:43 +0530

    implemented viewsets and authentication local nd global

---
+++ testapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from testapp.models import Employee
+from rest_framework.viewsets import ModelViewSet
+from testapp.serializers import EmployeeSerializer
+from rest_framework import permissions
+from rest_framework import authentication
+
+
+# Create your views here.
+
+class EmployeeCRUD(ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    # authentication_classes = [authentication.TokenAuthentication]
+    # permission_classes = [permissions.IsAuthenticated]
